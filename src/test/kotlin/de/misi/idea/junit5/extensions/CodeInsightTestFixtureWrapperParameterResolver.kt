f591b77ac969f07caeb6c0b11eb7f5c6755bd645
Michael Siepmann <misi1967@yahoo.de>
2017-08-08 00:04:13 +0200

    Simple optimizations

--- src/test/kotlin/de/misi/idea/junit5/extensions/CodeInsightTestFixtureWrapperParameterResolver.kt
+++ src/test/kotlin/de/misi/idea/junit5/extensions/CodeInsightTestFixtureWrapperParameterResolver.kt
@@ -10,31 +10,35 @@ import org.junit.jupiter.api.extension.ParameterResolver
 
 class CodeInsightTestFixtureWrapperParameterResolver : ParameterResolver, AfterEachCallback {
 
-    private var fixture: CodeInsightTestFixture? = null
-
     override fun supportsParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext) = parameterContext.isOfType(CodeInsightTestFixtureWrapper::class.java)
 
     override fun resolveParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Any? {
         val annotation = extensionContext.getAnnotation(FixtureTestcase::class.java)
         if (annotation != null) {
             val testFixtureBuilder = IdeaTestFixtureFactory.getFixtureFactory().createFixtureBuilder(extensionContext.requiredTestMethod?.name ?: "unknown")
-            fixture = JavaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(testFixtureBuilder.fixture)
-            fixture?.initialize(testFixtureBuilder, annotation.dataPath)
+            val fixture = JavaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(testFixtureBuilder.fixture)
+            with(fixture) {
+                testDataPath = annotation.dataPath
+                val builder = testFixtureBuilder.addModule(JavaModuleFixtureBuilder::class.java)
+                builder.addContentRoot(tempDirPath).addSourceRoot("")
+                builder.setMockJdkLevel(JavaModuleFixtureBuilder.MockJdkLevel.jdk15)
+                setUp()
+            }
+            extensionContext.store.put(STORE_KEY, fixture)
             return CodeInsightTestFixtureWrapper(fixture!!, annotation.beforeSuffix, annotation.afterSuffix)
         }
         return null
     }
 
-    private fun CodeInsightTestFixture.initialize(testFixtureBuilder: TestFixtureBuilder<IdeaProjectTestFixture>, path: String) {
-        testDataPath = path
-        val builder = testFixtureBuilder.addModule(JavaModuleFixtureBuilder::class.java)
-        builder.addContentRoot(tempDirPath).addSourceRoot("")
-        builder.setMockJdkLevel(JavaModuleFixtureBuilder.MockJdkLevel.jdk15)
-        setUp()
+    override fun afterEach(context: ExtensionContext) {
+        val store = context.store
+        (store.get(STORE_KEY) as CodeInsightTestFixture?)?.tearDown()
+        store.remove(STORE_KEY)
     }
 
-    override fun afterEach(context: ExtensionContext?) {
-        fixture?.tearDown()
-        fixture = null
+    companion object {
+
+        const val STORE_KEY = "de.misi.idea.junit5.fixture"
+
     }
 }
