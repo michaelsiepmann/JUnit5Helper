5bd429e54263e2aeedc10aab904949137d6126d7
Michael Siepmann <michael.siepmann@swr.de>
2017-08-14 15:42:33 +0200

    New Testcase

---
+++ src/test/resources/testData/surroundassertall/Surround with assertAll, with statement on multiple lines and mulltiline selection.before.template.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class X {
+    @Test
+    void f() {
+        String value = "Hello";
+<selection><caret>        assertEquals(5,
+                value.length);
+        assertEquals("H",
+                value.charAt(0));</selection>
+    }
+}
