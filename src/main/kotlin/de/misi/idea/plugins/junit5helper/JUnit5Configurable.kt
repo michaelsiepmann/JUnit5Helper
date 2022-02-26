0abee2e2329b0282bd0983fd4353f9580cc3a8a5
Michael Siepmann <misi1967@yahoo.de>
2022-02-27 00:39:43 +0100

    Bugfix

--- src/main/kotlin/de/misi/idea/plugins/junit5helper/JUnit5Configurable.kt
+++ src/main/kotlin/de/misi/idea/plugins/junit5helper/JUnit5Configurable.kt
@@ -2,34 +2,34 @@ package de.misi.idea.plugins.junit5helper
 
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
-import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.dsl.builder.panel
 import javax.swing.JComponent
 
 class JUnit5Configurable(private val project: Project) : Configurable {
 
-    private var addDisplayName = false
+    private lateinit var addDisplayName: JBCheckBox
 
     override fun createComponent(): JComponent {
         val bundle = JUnit5HelperBundle.INSTANCE
         val configuration = JUnit5WorkspaceConfiguration.getInstance(project)
-        addDisplayName = configuration.addDisplayName ?: false
         return panel {
             row {
-                checkBox(bundle.getMessage("configuration.addDisplayName"))
-                    .bindSelected(::addDisplayName)
+                addDisplayName = checkBox(bundle.getMessage("configuration.addDisplayName"))
+                    .component
+                addDisplayName.isSelected = configuration.addDisplayName ?: false
             }
         }
     }
 
     override fun isModified(): Boolean {
         val configuration = JUnit5WorkspaceConfiguration.getInstance(project)
-        return configuration.addDisplayName != addDisplayName
+        return configuration.addDisplayName != addDisplayName.isSelected
     }
 
     override fun apply() {
         val configuration = JUnit5WorkspaceConfiguration.getInstance(project)
-        configuration.addDisplayName = addDisplayName
+        configuration.addDisplayName = addDisplayName.isSelected
     }
 
     override fun getDisplayName() = "JUnit5 Helper"
