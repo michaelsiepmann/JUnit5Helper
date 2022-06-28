6a674943aa7815a1301e600a73374b98b3b4d793
Michael Siepmann <michael.siepmann@swr.de>
2022-06-28 16:48:31 +0200

    Versions updated.

--- src/main/kotlin/de/misi/idea/plugins/junit5helper/livetemplates/JUnit5Context.kt
+++ src/main/kotlin/de/misi/idea/plugins/junit5helper/livetemplates/JUnit5Context.kt
@@ -1,20 +1,23 @@
 package de.misi.idea.plugins.junit5helper.livetemplates
 
+import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.lang.java.JavaLanguage
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiUtilCore.getLanguageAtOffset
 import com.intellij.psi.util.parentOfType
 import de.misi.idea.plugins.junit5helper.isTestFile
 
 class JUnit5Context : TemplateContextType("JUNIT5", "JUnit 5") {
 
-    override fun isInContext(file: PsiFile, offset: Int) =
-        when {
+    override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+        val file = templateActionContext.file
+        val offset = templateActionContext.startOffset
+        return when {
             getLanguageAtOffset(file, offset).isKindOf(JavaLanguage.INSTANCE) ->
                 file.findElementAt(offset)?.parentOfType<PsiClass>()?.isTestFile() ?: false
             else ->
                 false
         }
+    }
 }
